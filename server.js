6c800ab28f3a7e11aa8aa5f1d14ba59a537321aa
ToniJ-22 <rachelrene1524@gmail.com>
2026-03-11 13:51:22 -0400

    ssd

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -89,82 +88,7 @@ app.delete("/api/meals/:id", (req, res) => {
   res.json({ message: "Meal deleted", data: deleted[0] });
 });
 
-app.post("/api/suggestions", async (req, res) => {
-  const { query } = req.body;
 
-  if (!query) {
-    return res.status(400).json({ message: "Query required" });
-  }
-
-  try {
-    const response = await axios.get(
-      `https://api.nal.usda.gov/fdc/v1/foods/search`,
-      {
-        params: {
-        api_key: process.env.API_KEY,
-        query,
-        pageSize: 5,
-      }
-      }
-    );
-
-    const foods = response.data.foods;
-
-    if (!foods) return res.json([]);
-
-    const suggestions = foods.map(food => food.description);
-
-    res.json(suggestions);
-
-  } catch (error) {
-    console.error("Suggestion error:", error.message);
-    res.status(500).json({ message: "Suggestion failed" });
-  }
-});
-app.post("/api/nutrition", async (req, res) => {
-  const { food } = req.body;
-
-  if (!food) {
-    return res.status(400).json({ message: "Food required" });
-  }
-
-  try {
-    const response = await axios.post(
-      `https://api.nal.usda.gov/fdc/v1/foods/search`,
-      {
-         params: {
-        api_key: process.env.API_KEY,
-        query:food,
-        pageSize: 1,
-         }
-      }
-    );
-
-    const foods = response.data.foods;
-    if (!foods || foods.length === 0) {
-      return res.status(404).json({ message: "Food not found" });
-    }
-
-    const nutrients = foods[0].foodNutrients;
-
-    const getNutrient = (name) => {
-      const nutrient = nutrients.find(n =>
-        n.nutrientName?.toLowerCase().includes(name.toLowerCase())
-      );
-      return nutrient?.value || 0;
-    };
-
-    res.json({
-      carbs: getNutrient("Carbohydrate"),
-      sugars: getNutrient("Sugars"),
-      protein: getNutrient("Protein"),
-    });
-
-  } catch (error) {
-    console.error("Nutrition error:", error.message);
-    res.status(500).json({ message: "Nutrition lookup failed" });
-  }
-});
 app.get("/", (req, res) => {
   res.send("Health Is Wealth API is running");
 });
